63f68c30c69108ef7fa198498866e80ad4704c09
Nikhil Arora <narora200@gmail.com>
2017-07-14 12:45:46 +0530

    Back-end: cors middleware added

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-const indexRouter = require('./routes');
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const indexRouter = require('./routes');
+
 
 app.use('/api', indexRouter);
 
