3521dcdaebbd1de2bee92d629243e77cdd80e9dd
Nikhil Arora <narora200@gmail.com>
2017-07-14 11:13:23 +0530

    Front-end: Boilerplate

---
+++ front-end/README.md
@@ -0,0 +1,1 @@
+# To-Do List (Front End)
---
+++ front-end/dev/js/client.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from "react-dom";
+
+
+const node = document.getElementById('root');
+
+ReactDOM.render(<div>Hello World</div>, node);
