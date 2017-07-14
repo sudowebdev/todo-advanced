c97048998c0856320bd68e7617b85c92c6e90829
Nikhil Arora <narora200@gmail.com>
2017-07-14 09:48:25 +0530

    Back-end: Retrieve all todos GET request working

--- back-end/routes/todo.js
+++ back-end/routes/todo.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 
+const Todo = require('../db/models').models.Todo;
 
 router.get('/', (req, res) => {
-	res.send('You are on /api/todo');
+	Todo.findAll({})
+	.then((todos) => {
+		res.status(200).send(todos);
+	})
+	.catch((err) => {
+		console.log("Error retrieving todos: ", err);
+		res.status(500).send("There was an error retrieving todos");
+	})
 })
 
 
