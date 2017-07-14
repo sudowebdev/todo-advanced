3933df52b306e1e0ebf017e3bfea1b9c32dc656e
Nikhil Arora <narora200@gmail.com>
2017-07-14 12:30:27 +0530

    Front-end: sending xhr request from axios in actions/todoActions.js

--- front-end/dev/js/actions/todoActions.js
+++ front-end/dev/js/actions/todoActions.js
@@ -3,9 +3,15 @@ import axios from 'axios';
 export function fetchTodo(){
 	return (dispatch) => {
 			
-			dispatch({type: 'FETCH_TODO', payload: '123'});
+			dispatch({type: 'FETCH_TODO_PENDING', payload: 'In Progress'});
 
-			dispatch({type: 'FETCH_TODO', payload: '456'});
+			axios.get('http://localhost:3333/api/todo')
+				.then((response) => {
+					dispatch({type: 'FETCH_TODO_FULFILLED', payload: response.data})
+				})
+				.catch((err) => {
+					dispatch({type: 'FETCH_TODO_REJECTED', payload: response.statusText })
+				})
 
 
 		}
